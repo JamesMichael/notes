dd01ad9e1faed95a22341138f783f1fd936fdcb2
James Michael <jamesamichael@gmail.com>
2015-03-07 17:52:11 +0000

    Hook up toolbar settings button to settings dialog

--- www/scripts/notes.js
+++ www/scripts/notes.js
@@ -1,1 +1,9 @@
-// Notes.js
+require('babelify/polyfill');
+import Dialog from './dialog';
+
+window.onload = function() {
+    let settings_dialog = new Dialog('dialog_settings');
+
+    let toolbar_settings_button = document.getElementById('toolbar_settings');
+    toolbar_settings_button.addEventListener('click', e => settings_dialog.open());
+}
