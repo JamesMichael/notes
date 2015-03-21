1926e2b7419bd945f52ce3aba9afc04df19b1363
James Michael <jamesamichael@gmail.com>
2015-03-21 19:51:26 +0000

    Add additional methods to ListViewDataSource
    * these will be needed by the ListView class to display all
      elements and respond to user interaction
        - create
        - iterator
        - contains
        - get
        - move_up
        - move_down
        - remove
    * currently methods are empty, they are designed to be overridden
      in a subclass

--- www/scripts/listview_datasource.js
+++ www/scripts/listview_datasource.js
@@ -5,6 +5,47 @@ class ListViewDataSource {
 
     get name()     { return this._name; }
     set name(name) { this._name = name; }
+
+    // create a new element
+    // add the new element to the datasource
+    // return the new element
+    create() {
+        return undefined;
+    }
+
+    // return an identifier for all entries in the list
+    [Symbol.iterator]() {
+        return {
+            next: () => {
+                return { value: undefined, done: true };
+            }
+        };
+    }
+
+    // return true iff the list contains an element
+    // with the identifier
+    contains(id) {
+        return false;
+    }
+
+    // return the element of the list with the given
+    // identifier, or undefined if it does not contain
+    // the the named element
+    get(id) {
+        return undefined;
+    }
+
+    // moves the named element up |amount| positions
+    move_up(id, amount = 1) {
+    }
+
+    // moves the named element down |amount| positions
+    move_down(id, amount = 1) {
+    }
+
+    // removes the element with the given id
+    remove(id) {
+    }
 }
 
 module.exports = ListViewDataSource;
