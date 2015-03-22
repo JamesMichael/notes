bc2dd4c0d268a8ba9db81050ee14df5642ab0169
James Michael <jamesamichael@gmail.com>
2015-03-22 15:39:28 +0000

    Display items from datasource when creating a ListView
    * iterates over datasource items, adds them to a document fragment
    * appends document fragment to template before it's rendered
    * clears the template after it has been cloned to 'reset' for the
      next render

--- www/scripts/listview.js
+++ www/scripts/listview.js
@@ -15,13 +15,23 @@ class ListView {
         this.element = element;
         this.datasource = datasource;
 
+        let list_fragment = document.createDocumentFragment();
+        for (let project_id of this.datasource) {
+            let li = document.createElement('li');
+            li.textContent = this.datasource.get(project_id);
+            list_fragment.appendChild(li);
+        }
+
         // copy the template, add data, and render into element
         let name = datasource.name;
         let template = document.getElementById(templateid);
         template.content.querySelector('.listview-search input').placeholder = `Search ${name}`;
+        template.content.querySelector('.listview-items').appendChild(list_fragment);
 
         let clone = document.importNode(template.content, true);
         element.appendChild(clone);
+
+        template.content.querySelector('.listview-items').innerHTML = '';
     }
 
     // called when the listview should be selected
